76c73a7a1609627a7e26b3b7c9842cd73e873f6e
p22dhealy <p22dhealy@fairfieldprep.org>
2021-11-15 10:14:10 -0500

    Submit hw 6-2

---
+++ hw6-2.py
@@ -0,0 +1,32 @@
+#author" DMH 11/15/21
+
+#Question 1
+list = [0,1,2,3,4,5,6,7]
+
+#Question 2
+print(len(list))
+
+#Question 3
+print(list[3])
+
+#Question 4
+print(5 not in list)
+
+#Question 5
+print(10 not in list)
+
+#Question 6
+names = ['Andrew','Bill','Charlie','Declan']
+
+#Question 7
+print(names[3])
+
+#Question 8
+print(names * 3)
+
+#Question 9
+combine = list + names
+
+#Question 10 
+print(len(combine))
+
